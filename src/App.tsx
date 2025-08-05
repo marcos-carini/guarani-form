849767482b3e8f1ae073cc038bc5528b1664ed8b
Marcos Vinicius <marcos.carini22@gmail.com>
2025-08-04 22:28:07 -0300

    feat: add shadcn components and color mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Leaf } from "lucide-react"
+import { Card, CardHeader } from "./components/ui/card"
+import { ModeToggle } from "./components/mode-toggle"
 
-import { Button } from './components/ui/button'
+
 
 function App() {
   
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="min-h-screen bg-gradient-to-br from-neutral-50 dark:from-neutral-950 via-neutral-100 dark:via-neutral-900 to-emerald-100 dark:to-emerald-950 flex items-center justify-center p-4">
+      <Card className="w-full max-w-4xl">
+        <CardHeader className="pb-6">
+          <div className="flex items-center gap-3">
+            <div className="w-8 h-8 bg-green-600 rounded-lg flex items-center justify-center">
+              <Leaf className="w-5 h-5 text-white" />
+            </div>
+            <h1 className="text-xl font-semibold">Guarani Sistemas</h1>
+            <ModeToggle/>
+          </div>
+        </CardHeader>
+      </Card>
     </div>
   )
 }
